6bfd28b5051e431b117caf43c830222ee73a9def
Kseniya <kseniya.panasyuk@gmail.com>
2016-08-08 21:51:14 -0400

    add simple editor

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,24 @@
 import { Component } from '@angular/core';
+export class Card {
+    id: number;
+    name: string;
+}
 @Component({
-    selector: 'my-app',
-    template: '<h1>Anki</h1>'
+selector: 'my-app',
+template: `
+        <h1>{{title}}</h1>
+        <h2>{{card.name}} details!</h2>
+        <div><label>id: </label>{{card.id}}</div>
+        <div>
+            <label>name: </label>
+            <input [(ngModel)]="card.name" placeholder="name">
+        </div>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+title = 'Anki';
+card: Card = {
+id: 1,
+name: 'Java'
+}
+}
